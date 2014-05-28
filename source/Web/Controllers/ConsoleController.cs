2d5045ee7bf8c2c0a208e821c9a894bbacf29b68
Alexander Groß <agross@therightstuff.de>
2014-05-28 09:54:31 +0200

    Initial commit, already got SignalR running

---
+++ source/Web/Controllers/ConsoleController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+using Microsoft.AspNet.SignalR;
+
+namespace Web.Controllers
+{
+  public class ConsoleController : ApiController
+  {
+    public void Post([FromBody]string line)
+    {
+      GlobalHost.ConnectionManager.GetHubContext<ConsoleHub>().Clients.All.broadcast(line);
+    }
+  }
+}
---
+++ source/Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+using Owin;
+
+namespace Web
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      app.MapSignalR();
+
+      GlobalConfiguration.Configure(x => x.Routes.MapHttpRoute(name: "API Default",
+                                                               routeTemplate: "api/{controller}/{id}",
+                                                               defaults: new { id = RouteParameter.Optional }
+                                           ));
+    }
+  }
+}
