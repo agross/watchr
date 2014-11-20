8cb94cf1f32df134a29d2a1cea9c0bbb90f12a91
Alexander Groß <agross@grossweber.com>
2014-11-20 23:49:29 +0100

    Fixed test.cmd paths

--- test.cmd
+++ test.cmd
@@ -1,2 +1,2 @@
-packages\NUnit.Runners\tools\nunit-console.exe /noresult source\Client.Console\bin\Debug\Client.Console.exe
+packages\NUnit.Runners\tools\nunit-console.exe /noresult source\Client\bin\Debug\Client.dll
 packages\Chutzpah\tools\chutzpah.console.exe /failOnError /showFailureReport /path source\Web.Tests
