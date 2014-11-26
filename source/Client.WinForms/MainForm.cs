08e03346042669dc991bf0adf0654e1a43da6d23
Alexander Groß <agross@therightstuff.de>
2014-11-26 20:28:10 +0100

    Adding WinForms client with simple notification icon

---
+++ source/Client.WinForms/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+using Client.WinForms.Properties;
+
+namespace Client.WinForms
+{
+  partial class MainForm : Form, IMainForm
+  {
+    public MainForm()
+    {
+      InitializeComponent();
+    }
+
+    public void SetState(ConnectionState connectionState)
+    {
+      notificationIcon.Text = connectionState.ToString();
+      notificationIcon.Icon = (Icon) Resources.ResourceManager.GetObject(connectionState.ToString());
+    }
+  }
+
+  interface IMainForm
+  {
+    void SetState(ConnectionState connectionState);
+  }
+
+  enum ConnectionState
+  {
+    Disconnected,
+    Connecting,
+    Connected
+  }
+}
