8f3e5e654cbcd6a4ad98b25df9355cc4bbc15f45
Alexander Groß <agross@therightstuff.de>
2015-11-16 16:00:32 +0100

    Removing SSL CACerts download, no longer needed with updated Rubygems

--- bundle.cmd
+++ bundle.cmd
@@ -2,17 +2,6 @@
 setlocal
 
 set LANG=en_US.UTF-8
-set SSL_CERT_FILE=%TEMP%\cacert.pem
-set DOWNLOAD_CA_BUNDLE="require 'net/http'; Net::HTTP.start('curl.haxx.se') { |http| resp = http.get('/ca/cacert.pem'); abort 'Error downloading CA bundle: ' + resp.code unless resp.code == '200'; open(ENV['SSL_CERT_FILE'], 'wb') { |file| file.write(resp.body) }; }"
-
-if not exist "%SSL_CERT_FILE%" (
-  echo Downloading latest CA bundle...
-  ruby -e %DOWNLOAD_CA_BUNDLE%
-
-  if errorlevel 1 (
-    exit /b %errorlevel%
-  )
-)
 
 call gem.bat which bundler > NUL 2>&1
 if errorlevel 1 (
