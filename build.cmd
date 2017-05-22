7e1192b1bd515d3f31e79bbc98fd8c989f40cf3f
Alexander Groß <agross@therightstuff.de>
2017-05-22 11:59:07 +0200

    Call ruby gems without bat extension

--- build.cmd
+++ build.cmd
@@ -1,10 +1,17 @@
 @echo off
 setlocal
 
+pushd "%~dp0"
+
+chcp 65001 > NUL
+
+for %%a in (.) do set dirname=%%~na
+set args=%*
 set LANG=en_US.UTF-8
 
 :build
 cls
+title %dirname%: %~n0 %args% (in %~dp0)
 
 set CI=
 if defined TEAMCITY_PROJECT_NAME set CI=--without development
@@ -13,9 +20,7 @@ call bundle.cmd check
 if errorlevel 1 call bundle.cmd install %CI%
 if errorlevel 1 goto wait
 
-cls
-
-call bundle.cmd exec rake %*
+call bundle.cmd exec rake %args%
 
 :wait
 rem Bail if we're running a TeamCity build or from Visual Studio.
@@ -23,10 +28,9 @@ if defined TEAMCITY_PROJECT_NAME goto quit
 if defined VS_BUILD_EVENT goto quit
 
 rem Loop the build script.
-set choice=nothing
-echo (Q)uit, (Enter) runs the build again
-set /P choice=
-if /i "%choice%"=="Q" goto quit
+echo (Q)uit, [optional new build target] followed by (Enter) runs the build again
+set /p args=
+if /i "%args%"=="Q" goto quit
 
 goto build
 
--- bundle.cmd
+++ bundle.cmd
@@ -1,14 +1,16 @@
 @echo off
 setlocal
 
+pushd "%~dp0"
+
+chcp 65001 > NUL
+
 if defined TEAMCITY_PROJECT_NAME echo ##teamcity[blockOpened name='%0 %*']
 
-set LANG=en_US.UTF-8
-
-call gem.bat which bundler > NUL 2>&1
+call gem which bundler > NUL 2>&1
 if errorlevel 1 (
   echo Installing bundler...
-  call gem.bat install bundler --no-ri --no-rdoc
+  call gem install bundler --no-ri --no-rdoc
 )
 
 call bundle.bat %*
