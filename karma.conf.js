26c275c119c25b2e7ca3e4eb90159d99e0294df4
Alexander Groß <agross@therightstuff.de>
2023-04-02 02:32:42 +0200

    Enable Chromium headless for tests

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: [ process.env.TEAMCITY_PROJECT_NAME ? 'ChromiumHeadless' : 'ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
